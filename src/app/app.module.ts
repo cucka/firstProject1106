ad08ca44712050d781fb667c5ca29c3c8a7da5b8
Tsvetelina Dimitrova <tsvetelina.milcheva@gmail.com>
2021-06-11 13:23:40 +0300

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './button/button.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
